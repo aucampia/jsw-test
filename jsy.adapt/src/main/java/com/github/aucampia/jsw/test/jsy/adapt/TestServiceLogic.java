540d160a0ec5ed776e9e834144b5b34f3b7f0d67
Iwan Aucamp <aucampia@gmail.com>
2014-08-25 13:54:16 +0200

    Initial attempt at JavaEE adapter and EAR assembly

---
+++ jsy.adapt/src/main/java/com/github/aucampia/jsw/test/jsy/adapt/TestServiceLogic.java
@@ -0,0 +1,35 @@
+package com.github.aucampia.jsw.test.jsy.adapt;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// http://docs.oracle.com/javaee/7/api/index.html?javax/enterprise/context/ApplicationScoped.html
+import javax.enterprise.context.ApplicationScoped;
+// http://docs.oracle.com/javaee/7/api/index.html?javax/annotation/PreDestroy.html
+import javax.annotation.PreDestroy;
+// http://docs.oracle.com/javaee/7/api/index.html?javax/annotation/PostConstruct.html
+import javax.annotation.PostConstruct;
+// http://docs.oracle.com/javaee/7/api/index.html?javax/inject/Named.html
+import javax.inject.Named;
+
+@Named
+@ApplicationScoped
+public class TestServiceLogic
+	extends com.github.aucampia.jsw.test.logic.comp.TestServiceLogic
+{
+	private static final Logger LOG = LoggerFactory.getLogger( TestServiceLogic.class );
+
+	@PostConstruct
+	void postConstruct()
+	{
+		LOG.info( "jsw.test.jsy.adapt.TestServiceLogic.postConstruct: ..." );
+		super.init();
+	}
+
+	@PreDestroy
+	void preDestroy()
+	{
+		LOG.info( "jsw.test.jsy.adapt.TestServiceLogic.preDestroy: ..." );
+		super.destroy();
+	}
+}
